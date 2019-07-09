ef6f6f675f7f2dffccd7259ef52582c0ec0d246b
Filip Kublin <fpkublin@gmail.com>
2019-07-09 17:19:46 +0200

    Rebuilt serviceClass; added corresponding tests

--- serviceClass.js
+++ serviceClass.js
@@ -1,13 +1,44 @@
+const {ServiceStatus} = require('./symbolConstants')
+const assert = require('assert')
+
 class serviceClass {
-    
-    constructor(field)
-    {
-        this.field = field
+    constructor(config){
+        this._status = ServiceStatus.STARTED
+        this.handlers = []
     }
-    start(config){}
-    stop(reason){}
-    request(params){}
-    send(data){}
+
+    get status(){
+        return this._status
+    }
+
+    stop(){
+        this._status = ServiceStatus.STOPPED
+        this.handlers.length = 0
+    }
+
+    subscribe(handler){
+        assert(typeof handler === 'function', 'Handler is not a function')
+        this.handlers.push(handler)
+    }
+
+    send(data){
+        assert(typeof data === 'object' &&  data !== null && typeof data !== 'function', 'Data is not an object')
+        return true
+    }
+
+    receive(data){
+        for (let fun of this.handlers){
+            try
+            {
+                fun(data)    
+            }
+            catch
+            {
+                throw('Unable to complete function')
+            }
+        }
+    }
+    
 }
 
 exports.ServiceClass = serviceClass
--- test/serviceTest.js
+++ test/serviceTest.js
@@ -1,22 +1,59 @@
-const assert = require('assert')
-const service = require('../serviceClass')
 const {ServiceClass} = require('../serviceClass')
-const chai = require('chai')
+const {assert} = require('chai')
+const {ServiceStatus} = require('../symbolConstants')
 
 describe('Environment', ()=>{
     it('is ready', ()=>{
-        assert(true)
+        assert.isTrue(true);
     })
 })
 
 describe('Service class', ()=>{
     it('is a function', ()=>{
-        
-        assert(ServiceClass instanceof Function)
-        
+        assert(ServiceClass instanceof Function)        
     })
+
     it('creates an object', ()=>{
         const instance = new ServiceClass('test')
         assert(instance instanceof ServiceClass)
+    })
+
+    it('get status returns STARTED', ()=>{
+        const instance = new ServiceClass('test')
+        assert.equal(instance.status, ServiceStatus.STARTED, 'should be STARTED')
+    })
+
+    it('subscribe checks if handlers are functions', ()=>{
+        //const testObject = 'value'
+        const instance = new ServiceClass('test value')
+        const tester = () => instance.subscribe( {})
+        assert.throws(tester, 'Handler is not a function')
+    })
+
+    it('subscribe adds handlers correctly', ()=>{
+        function handler1(){}
+        const instance = new ServiceClass('test value')
+        instance.subscribe(handler1)
+        assert.isTrue(instance.handlers.includes(handler1), 'Handlers should include handler1')
+    })
+
+    it('send checks if data is an object', ()=> {
+        const instance = new ServiceClass('test value')
+        const tester = () => instance.send(()=>{})
+        assert.throws(tester, 'Data is not an object')
+    })
+
+    it('send returns true if an object is passed', ()=>{
+        const instance = new ServiceClass('test value')
+        assert(instance.send({}), 'Send should return true')
+    })
+
+    it('receive runs functions provided by handlers', ()=>{
+        const instance = new ServiceClass('test value')
+        function handler1(){return true}
+        function handler2(){throws('Exception')}
+        instance.subscribe(handler1)
+        instance.subscribe(handler2)
+        
     })
 })
