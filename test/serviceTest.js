f7d855642f3508d69650629ad9798f4aa1782983
Filip Kublin <fpkublin@gmail.com>
2019-07-10 11:41:10 +0200

    Updated testing

--- test/serviceTest.js
+++ test/serviceTest.js
@@ -14,46 +14,52 @@ describe('Service class', ()=>{
     })
 
     it('creates an object', ()=>{
-        const instance = new ServiceClass('test')
+        const instance = new ServiceClass()
         assert(instance instanceof ServiceClass)
     })
 
     it('get status returns STARTED', ()=>{
-        const instance = new ServiceClass('test')
+        const instance = new ServiceClass()
         assert.equal(instance.status, ServiceStatus.STARTED, 'should be STARTED')
     })
 
     it('subscribe checks if handlers are functions', ()=>{
         //const testObject = 'value'
-        const instance = new ServiceClass('test value')
+        const instance = new ServiceClass()
         const tester = () => instance.subscribe( {})
         assert.throws(tester, 'Handler is not a function')
     })
 
     it('subscribe adds handlers correctly', ()=>{
         function handler1(){}
-        const instance = new ServiceClass('test value')
+        const instance = new ServiceClass()
         instance.subscribe(handler1)
         assert.isTrue(instance.handlers.includes(handler1), 'Handlers should include handler1')
     })
 
     it('send checks if data is an object', ()=> {
-        const instance = new ServiceClass('test value')
+        const instance = new ServiceClass()
         const tester = () => instance.send(()=>{})
         assert.throws(tester, 'Data is not an object')
     })
 
     it('send returns true if an object is passed', ()=>{
-        const instance = new ServiceClass('test value')
+        const instance = new ServiceClass()
         assert(instance.send({}), 'Send should return true')
     })
 
     it('receive runs functions provided by handlers', ()=>{
-        const instance = new ServiceClass('test value')
-        function handler1(){return true}
-        function handler2(){throws('Exception')}
+        const instance = new ServiceClass()
+        function handler1(){}
+        function handler2(){
+            console.log('test')
+            throw(new Error('Exception'))
+        }
+        function handler3(){}
         instance.subscribe(handler1)
         instance.subscribe(handler2)
-        
+        instance.subscribe(handler3)
+        assert.isTrue(instance.receive({}), 'fuckup')
+
     })
 })
